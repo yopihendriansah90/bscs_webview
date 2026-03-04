90b60725d690c978dadd22a1f3cf213ba8229d66
yopihendriansah90 <yopihendriansah90@gmail.com>
2026-03-04 22:38:57 +0700

    bscs app

---
+++ android/app/src/main/kotlin/com/bscs/app/bscs/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.bscs.app.bscs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Be Smart Clean School (BSCS)',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const WebViewPage(),
+    );
+  }
+}
+
+class WebViewPage extends StatefulWidget {
+  const WebViewPage({super.key});
+
+  @override
+  State<WebViewPage> createState() => _WebViewPageState();
+}
+
+class _WebViewPageState extends State<WebViewPage> {
+  late final WebViewController _controller;
+  int _loadingProgress = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            setState(() {
+              _loadingProgress = progress;
+            });
+          },
+        ),
+      )
+      ..loadRequest(
+        Uri.parse('https://smada-tuban-clean-ops.vercel.app'),
+      );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (bool didPop, Object? result) async {
+        if (didPop) {
+          return;
+        }
+
+        if (await _controller.canGoBack()) {
+          await _controller.goBack();
+        } else if (context.mounted) {
+          Navigator.of(context).pop();
+        }
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: Stack(
+            children: <Widget>[
+              WebViewWidget(controller: _controller),
+              if (_loadingProgress < 100)
+                const LinearProgressIndicator(minHeight: 2),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
